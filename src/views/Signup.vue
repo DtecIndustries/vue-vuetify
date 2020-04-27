1b12552e0b7d9a0b2bcdc2f42e3457f01208088b
Joachim Defour <joachim.defour@icloud.com>
2020-04-27 17:06:16 +0200

    form validation

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -4,9 +4,15 @@
       <v-col>
         <h1>Signup</h1>
 
-        <v-form>
+        <v-form ref="signUpForm" v-model="formValidity">
           <!-- textfield -->
-          <v-text-field label="Email" type="email"></v-text-field>
+          <v-text-field 
+            label="Email" 
+            type="email"
+            v-model="email"
+            :rules="emailRules"
+            required
+          ></v-text-field>
 
           <!-- autocompletes -->
           <v-autocomplete label="Which browser do you use?" :items="browsers"></v-autocomplete>
@@ -19,10 +25,32 @@
           <v-date-picker v-model="birthday"></v-date-picker>
 
           <!-- checkbox -->
-          <v-checkbox label="Agree to terms & conditions"></v-checkbox>
+          <v-checkbox 
+            label="Agree to terms & conditions" 
+            v-model="agreeToTerms"
+            :rules="agreeToTermsRules"
+            required
+          ></v-checkbox>
 
           <!-- submit btn -->
-          <v-btn type="submit" color="primary">Submit</v-btn>
+          <v-btn 
+            type="submit" 
+            class="mr-4" 
+            color="primary"
+            :disabled="!formValidity"
+          >Submit</v-btn>
+
+          <!-- validate btn -->
+          <v-btn 
+            class="mr-4" 
+            color="success"
+            @click="validateForm"
+          >Validate Form</v-btn>
+
+          <!-- form reset -->
+          <v-btn class="mr-4" color="warning" @click="resetValidation">Reset validation</v-btn>
+          <v-btn color="error" @click="resetForm">Reset</v-btn>
+
         </v-form>
       </v-col>
     </v-row>
@@ -40,7 +68,32 @@ export default {
         'Edge',
         'Brave'
       ],
-      birthday: ''
+      birthday: '',
+      agreeToTerms: false,
+      agreeToTermsRules: [
+        value => !!value || 'You must agree to the terms and conditions to sign up for an account'
+      ],
+      email: '',
+      emailRules: [
+        value => !!value || 'Email is required.',
+        value => value.indexOf('@') !== 0 || 'Email should have a username.',
+        value => value.includes('@') || 'Email should include an @ symbol.',
+        value => value.indexOf('.') - value.indexOf('@') > 1 || 'Email should contain a valid domain.',
+        value => value.includes('.') || 'Email should include a period symbol.',
+        value => value.indexOf('.') <= value.length - 3 || 'Email should contain a valid domain extension.'
+      ],
+      formValidity: false
+    }
+  },
+  methods: {
+    resetValidation() {
+      this.$refs.signUpForm.resetValidation()
+    },
+    resetForm() {
+      this.$refs.signUpForm.reset()
+    },
+    validateForm() {
+      this.$refs.signUpForm.validate()
     }
   }
 }
