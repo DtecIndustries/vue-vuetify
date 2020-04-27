33f6695b4f1bec6b3916f20ccc3dcefb88e4604a
Joachim Defour <joachim.defour@icloud.com>
2020-04-27 17:36:18 +0200

    Show content on scroll in dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -34,7 +34,26 @@
       </v-col>
     </v-row>
 
-    <v-snackbar v-model="snackbar" :left="$vuetify.breakpoint.mdAndUp">
+    <v-row id="below-the-fold" v-intersect="showMoreContent">
+      <v-col cols="12" md="8">
+        <EmployeesTable :employees="employees" @select-employee="setEmployee" />
+      </v-col>
+      <v-col cols="12" md="4">
+        <EventTimeline :timeline="timeline" />
+      </v-col>
+    </v-row>
+
+    <v-row v-if="loadNewContent"  id="more-content">
+      <v-col>
+        <v-skeleton-loader
+          ref="skeleton"
+          type="table"
+          class="mx-auto"
+        ></v-skeleton-loader>
+      </v-col>
+    </v-row>
+
+    <v-snackbar v-model="snackbar" :left="$vuetify.breakpoint.lgAndUp">
       You have selected {{ selectedEmployee.name }},
       {{ selectedEmployee.title }}
       <v-btn color="pink" text @click="snackbar = false">
@@ -65,6 +84,7 @@ export default {
   },
   data() {
     return {
+      loadNewContent: false,
       employees: employeesData,
       sales: salesData,
       selectedEmployee: {
@@ -73,7 +93,7 @@ export default {
       },
       snackbar: false,
       statistics: statisticsData,
-      timeline: timelineData
+      timeline: timelineData,
     }
   },
   methods: {
@@ -81,6 +101,9 @@ export default {
       this.snackbar = true
       this.selectedEmployee.name = event.name
       this.selectedEmployee.title = event.title
+    },
+    showMoreContent(entries) {
+      this.loadNewContent = entries[0].isIntersecting
     }
   }
 }
